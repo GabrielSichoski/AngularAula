e30666b31450ef6406d84f768bbcdf273905f048
Gabriel Sichoski <104863390+GabrielSichoski@users.noreply.github.com>
2023-10-11 21:41:54 -0300

    Add files via upload

---
+++ src/app/calculadora/calculadora.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calculadora',
+  templateUrl: './calculadora.component.html',
+  styleUrls: ['./calculadora.component.css']
+})
+export class CalculadoraComponent {
+    num1: number = 0;
+    num2: number = 0;
+    operacao: String = '+'
+    constructor(){}
+    resultado: number = 0;
+    ngOnInit(): void{}
+
+    calcular(){
+      if(this.operacao == '+'){
+        this.resultado = this.num1 + this.num2;
+      }
+      else if (this.operacao == '-'){
+        this.resultado = this.num1 - this.num2;
+      }
+    }
+}
